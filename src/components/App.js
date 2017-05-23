23fb16af667a6f55e3a6caeb5948790e4fc30de6
Christian Duvholt <christian@duvholt.net>
2017-05-23 18:21:59 +0200

    Tweak design a little

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import css from './App.css';
 
 const App = () => (
   <div className={css.app}>
-    <h1 className={css.title}>Kaffe på Onlinekontoret</h1>
+    <h1 className={css.title}>Onlinekontoret</h1>
     <Coffee />
     <div className={css.credits}>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
   </div>
