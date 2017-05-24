47f1193496a849f06ffe348e06cf212e9b91f15a
Christian Duvholt <christian@duvholt.net>
2017-05-24 21:47:28 +0200

    Use prop-types because PropTypes from React is deprecated

--- src/components/Office.js
+++ src/components/Office.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { socketConnect } from 'socket.io-react';
 import Coffee from './Coffee';
 import Status from './Status';
