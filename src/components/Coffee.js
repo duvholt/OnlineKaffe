627c032270d2e09483905580d814e81f72fdb6c5
Christian Duvholt <christian@duvholt.net>
2017-05-29 14:42:11 +0200

    Fix time since coffee was made

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import css from './Coffee.css';
 
 
-const Coffee = ({ date, pots }) => (
-  <div className={css.container}>
-    <div className={css.date}>
-      { date !== null ?
-        `Kaffen ble laget for ${moment(date).fromNow()}`
-      :
-        'Kaffe har ikke blitt laget i dag'
-      }
-    </div>
-    <div className={css.pots}>{pots} kanner totalt i dag</div>
-    <div className={css.icon} />
-  </div>
-);
+class Coffee extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      timeSince: moment(props.date).fromNow(),
+    };
+
+    setInterval(this.tick.bind(this), 1000);
+  }
+
+  tick() {
+    this.setState({
+      timeSince: moment(this.props.date).fromNow(),
+    });
+  }
+
+  render() {
+    const { date, pots } = this.props;
+    const { timeSince } = this.state;
+    return (
+      <div className={css.container}>
+        <div className={css.date}>
+          { date !== null ?
+            `Kaffen ble laget for ${timeSince}`
+          :
+            'Kaffe har ikke blitt laget i dag'
+          }
+        </div>
+        <div className={css.pots}>{pots} kanner totalt i dag</div>
+        <div className={css.icon} />
+      </div>
+    );
+  }
+}
 
 Coffee.defaultProps = {
   date: null,
