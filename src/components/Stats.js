65c115cd60288ad236390ce38712d01c79dd5992
Christian Duvholt <christian@duvholt.net>
2017-06-01 19:01:37 +0200

    Less hacky config and proper tooltip

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -8,7 +8,7 @@ import { API_HOST, API_COFFEE } from '../constants';
 HeatMap(ReactHighcharts.Highcharts);
 HighchartsExporting(ReactHighcharts.Highcharts);
 
-const config = {
+const createHighcartConfig = (data) => ({
   chart: {
     type: 'heatmap',
     marginTop: 40,
@@ -46,38 +46,23 @@ const config = {
   },
 
   tooltip: {
-    formatter: function () {
-      return '<b>' + this.series.xAxis.categories[this.point.x] + '</b> sold <br><b>' +
-        this.point.value + '</b> items on <br><b>' + this.series.yAxis.categories[this.point.y] + '</b>'
+    formatter: function formatter() {
+      const hour = this.series.xAxis.categories[this.point.x];
+      const pots = this.point.value;
+      const weekday = this.series.yAxis.categories[this.point.y];
+      return `<b>${pots}</b> kaffekanner ble laget kl <b>${hour}</b> på <b>${weekday.toLowerCase()}</b>`;
     },
   },
 
   series: [{
-    name: 'Sales per employee',
     borderWidth: 1,
-    data: [
-      [0, 0, 10], [0, 1, 19], [0, 2, 8], [0, 3, 24], [0, 4, 67], [0, 5, 67], [0, 6, 67],
-      [1, 0, 92], [1, 1, 58], [1, 2, 78], [1, 3, 117], [1, 4, 48], [1, 5, 67], [1, 6, 67],
-      [2, 0, 35], [2, 1, 15], [2, 2, 123], [2, 3, 64], [2, 4, 52], [2, 5, 67], [2, 6, 67],
-      [3, 0, 72], [3, 1, 132], [3, 2, 114], [3, 3, 19], [3, 4, 16], [3, 5, 67], [3, 6, 67],
-      [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], [4, 4, 115], [4, 5, 67], [4, 6, 67],
-      [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120], [5, 5, 67], [5, 6, 67],
-      [6, 0, 13], [6, 1, 44], [6, 2, 88], [6, 3, 98], [6, 4, 96], [6, 5, 67], [6, 6, 67],
-      [7, 0, 31], [7, 1, 1], [7, 2, 82], [7, 3, 32], [7, 4, 30], [7, 5, 67], [7, 6, 67],
-      [8, 0, 85], [8, 1, 97], [8, 2, 123], [8, 3, 64], [8, 4, 84], [8, 5, 67], [8, 6, 67],
-      [9, 0, 47], [9, 1, 114], [9, 2, 31], [9, 3, 48], [9, 4, 91], [9, 5, 67], [9, 6, 67],
-      [10, 0, 47], [10, 1, 114], [10, 2, 31], [10, 3, 48], [10, 4, 91], [10, 5, 67], [10, 6, 67],
-      [11, 0, 47], [11, 1, 114], [11, 2, 31], [11, 3, 48], [11, 4, 91], [11, 5, 67], [11, 6, 67],
-      [12, 0, 47], [12, 1, 114], [12, 2, 31], [12, 3, 48], [12, 4, 91], [12, 5, 67], [12, 6, 67],
-      [13, 0, 47], [13, 1, 114], [13, 2, 31], [13, 3, 48], [13, 4, 91], [13, 5, 67], [13, 6, 67],
-      [14, 0, 47], [14, 1, 114], [14, 2, 31], [14, 3, 48], [14, 4, 91], [14, 5, 67], [14, 6, 67],
-    ],
+    data,
     dataLabels: {
       enabled: true,
       color: '#000000',
     },
   }],
-};
+});
 
 const groupPots = (pots) => {
   return pots.reduce((dates, potDate) => {
@@ -151,10 +136,9 @@ class Stats extends Component {
   render() {
     const { pots } = this.state;
     const data = highChartData(groupPots(pots));
-    config.series[0].data = data;
     return (
       <div style={{ marginTop: '2rem' }}>
-        <ReactHighcharts config={config} />
+        <ReactHighcharts config={createHighcartConfig(data)} />
       </div>
     );
   }
