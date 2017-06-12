d33ed51db5507f2ef5118eb087b9a75740f0f5e9
Christian Duvholt <christian@duvholt.net>
2017-06-12 22:02:42 +0200

    Rename Stats to CoffeeHeatmap

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Office from './Office';
-import Stats from './Stats';
+import CoffeeHeatmap from './CoffeeHeatmap';
 import css from './App.css';
 
 const App = () => (
   <div className={css.app}>
     <h1 className={css.title}>Onlinekontoret</h1>
     <Office name="online" />
-    <Stats name="online" />
+    <CoffeeHeatmap name="online" />
     <div className={css.credits}>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a></div>
   </div>
 );
